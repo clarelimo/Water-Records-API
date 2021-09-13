c52a052f4260b857188e3ffd99f5e9b2f59ce10a
clarelimo <clare.limo@student.moringaschool.com>
2021-09-13 14:34:42 +0300

    expenses model complete

--- water-records/src/main/java/models/Expenses.java
+++ water-records/src/main/java/models/Expenses.java
@@ -1,4 +1,53 @@
 package models;
 
+import java.util.Objects;
+
 public class Expenses {
+    private String description;
+    private int amount;
+    private int id;
+
+    public Expenses(String description, int amount) {
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Expenses expenses = (Expenses) o;
+        return amount == expenses.amount &&
+                id == expenses.id &&
+                description.equals(expenses.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(description, amount, id);
+    }
 }
---
+++ water-records/src/test/java/models/SalesTest.java
@@ -0,0 +1,83 @@
+package models;
+
+import org.junit.Test;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+import static org.junit.Assert.*;
+
+public class SalesTest {
+
+    @Test
+    public void getDateReturnsCorrectDate() {
+    }
+
+    @Test
+    public void setDate() {
+    }
+
+    @Test
+    public void getCashSalesReturnsCorrectCashSales() {
+    }
+
+    @Test
+    public void setCashSales() {
+    }
+
+    @Test
+    public void getMpesaSales() {
+    }
+
+    @Test
+    public void setMpesaSales() {
+    }
+
+    @Test
+    public void getLitresSold() {
+    }
+
+    @Test
+    public void setLitresSold() {
+    }
+
+    @Test
+    public void getEmptyBottlesSold() {
+    }
+
+    @Test
+    public void setEmptyBottlesSold() {
+    }
+
+    @Test
+    public void getTotalSales() {
+    }
+
+    @Test
+    public void getExpectedTotalSales() {
+    }
+
+    @Test
+    public void getMachineImage() {
+    }
+
+    @Test
+    public void setMachineImage() {
+    }
+
+    @Test
+    public void isWaterRefilled() {
+    }
+
+    @Test
+    public void setWaterRefilled() {
+    }
+
+    @Test
+    public void getBalance() {
+    }
+
+    @Test
+    public void setBalance() {
+    }
+}
